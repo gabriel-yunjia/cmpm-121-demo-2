c4977d14f939c0bec7aa590527b35a145c278f5e
gabriel-yunjia <gli79@ucsc.edu>
2023-10-25 00:10:21 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -32,54 +32,66 @@ const ctx = canvas.getContext("2d");
 
 // draw state variables
 let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
-const displayList: [number, number][][] = [];
-const drawPoints: [number, number][] = [];
-const redoStack: [number, number][][] = [];
+
+class MarkerLine {
+  private points: { x: number; y: number }[] = [];
+
+  constructor(initialX: number, initialY: number) {
+    this.points.push({ x: initialX, y: initialY });
+  }
+
+  drag(x: number, y: number) {
+    this.points.push({ x, y });
+  }
+
+  display(ctx: CanvasRenderingContext2D) {
+    if (this.points.length > 1) {
+      ctx.strokeStyle = "black";
+      ctx.lineJoin = "round";
+      ctx.lineWidth = 5;
+
+      ctx.beginPath();
+      ctx.moveTo(this.points[0].x, this.points[0].y);
+      for (let i = 1; i < this.points.length; i++) {
+        ctx.lineTo(this.points[i].x, this.points[i].y);
+      }
+      ctx.stroke();
+    }
+  }
+}
+
+interface DrawingData {
+  displayList: MarkerLine[];
+  redoStack: MarkerLine[];
+}
+
+const drawingData: DrawingData = { displayList: [], redoStack: [] };
+
 // Mouse click listeners
 canvas.addEventListener("mousedown", (e: MouseEvent) => {
   if (ctx) {
     isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
+    const currentLine = new MarkerLine(e.offsetX, e.offsetY);
+    drawingData.displayList.push(currentLine);
   }
 });
 
 canvas.addEventListener("mousemove", (e: MouseEvent) => {
-  if (ctx) {
-    draw(e);
+  if (ctx && isDrawing) {
+    const currentPath =
+      drawingData.displayList[drawingData.displayList.length - 1];
+    currentPath.drag(e.offsetX, e.offsetY);
+    redraw();
   }
 });
 
 canvas.addEventListener("mouseup", () => {
-  isDrawing = false;
-  // Add the current line to the display list
-  displayList.push([...drawPoints]);
-  // Dispatch the "drawing-changed" event with the display list
-  canvas.dispatchEvent(
-    new CustomEvent("drawing-changed", { detail: displayList })
-  );
-  // Clear the drawing points after dispatching the event
-  clearDrawingPoints();
+  if (isDrawing) {
+    isDrawing = false;
+  }
 });
 
 canvas.addEventListener("mouseout", () => (isDrawing = false));
-
-function draw(e: MouseEvent) {
-  if (ctx && isDrawing) {
-    const [x, y] = [e.offsetX, e.offsetY];
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(x, y);
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-    ctx.lineCap = "round";
-    ctx.stroke();
-    [lastX, lastY] = [x, y];
-    // Save the point to the drawingPoints array
-    drawPoints.push([x, y]);
-  }
-}
 
 // Create a container for buttons arranged horizontally
 const horizontalContainer = document.createElement("div");
@@ -128,26 +140,23 @@ redoButton.addEventListener("click", () => {
 
 // Function to undo the last line
 function undo() {
-  if (displayList.length > 0) {
-    const line = displayList.pop(); // Remove the last line from displayList
+  if (drawingData.displayList.length > 0) {
+    const line = drawingData.displayList.pop(); // Remove the last line from displayList
+
     if (line) {
-      redoStack.push(line); // Add the line to redoStack
-      canvas.dispatchEvent(
-        new CustomEvent("drawing-changed", { detail: displayList })
-      );
+      drawingData.redoStack.push(line); // Add the line to redoStack
+      redraw();
     }
   }
 }
 
 // Function to redo the last undone line
 function redo() {
-  if (redoStack.length > 0) {
-    const line = redoStack.pop(); // Remove the last line from redoStack
+  if (drawingData.displayList.length > 0) {
+    const line = drawingData.displayList.pop(); // Remove the last line from displayList
     if (line) {
-      displayList.push(line); // Add the line back to displayList
-      canvas.dispatchEvent(
-        new CustomEvent("drawing-changed", { detail: displayList })
-      );
+      drawingData.displayList.push(line); // Add the line back to displayList
+      redraw();
     }
   }
 }
@@ -155,40 +164,17 @@ function redo() {
 // Function to clear the canvas
 function clearCanvas() {
   if (ctx) {
+    drawingData.displayList = [];
+    drawingData.redoStack = [];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    displayList.length = 0;
-    clearDrawingPoints();
   }
 }
-
-// Function to clear the drawing points
-function clearDrawingPoints() {
-  drawPoints.length = 0;
-}
-
-//observer
-canvas.addEventListener("drawing-changed", (e: Event) => {
-  if (ctx) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the entire canvas
-
-    // Type assertion
-    const customEvent = e as CustomEvent<[number, number][][]>; // Make sure it's an array of arrays
-    const displayList: [number, number][][] = customEvent.detail;
-
-    // Redraw
-    for (const points of displayList) {
-      ctx.beginPath();
-      points.forEach(([x, y], index) => {
-        if (index === 0) {
-          ctx.moveTo(x, y);
-        } else {
-          ctx.lineTo(x, y);
-        }
-      });
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 2;
-      ctx.lineCap = "round";
-      ctx.stroke();
+function redraw() {
+  if (drawingData?.displayList && ctx) {
+    const paths = drawingData.displayList;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const path of paths) {
+      path.display(ctx);
     }
   }
-});
+}
